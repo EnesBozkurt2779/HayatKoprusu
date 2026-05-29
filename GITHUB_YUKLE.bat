8453403c7e5acf96c3a1462eaef68e1cdfb57fa8
Enes Bozkurt <enesbzkrt009@gmail.com>
2026-05-29 22:48:24 +0300

    Initial commit

--- GITHUB_YUKLE.bat
+++ GITHUB_YUKLE.bat
@@ -1,8 +1,19 @@
 @echo off
-set /p repo_url="Lutfen GitHub depo (repository) linkinizi yapistirin (ornek: https://github.com/kullanici/proje.git): "
+:: Change directory to the script's location
+cd /d "%~dp0"
+
+echo Hayat Koprusu - GitHub Yukleme Araci
+echo -----------------------------------
+set /p repo_url="Lutfen GitHub depo linkinizi yapistirin: "
+
+:: Initialize and upload
+git init
+git add .
+git commit -m "Initial commit"
 git remote add origin %repo_url%
 git branch -M main
-git push -u origin main
+git push -u origin main -f
+
 echo.
 echo Islem tamamlandi! GitHub sayfanizdaki 'Actions' sekmesine giderek build islemini takip edebilirsiniz.
 pause
